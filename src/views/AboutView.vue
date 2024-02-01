de76c52e126027c02c27c2aa984c58c57a7a19b6
Serhii Lesnik <Serhii.Lesnik@nice.com>
2024-02-01 22:23:05 +0200

    Update About page

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,15 +1,23 @@
+<script setup lang="ts">
+import { reactive } from 'vue';
+
+const state = reactive({
+  vehicleName: ''
+});
+
+function addVehicle(){
+  alert(state.vehicleName)
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <input type="text" v-model="state.vehicleName">
+    <button @click="addVehicle">Add vehicle</button>
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
+
 </style>
